041d673fd8ba859e198addebb4cba0f4d7218f54
Matthew Pence <matt@ohiosveryown.co>
2019-08-11 15:24:39 -0400

    Updatess

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,18 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        supersymmetry-2020
-      </h1>
-      <h2 class="subtitle">
-        Supersymmetry is a photo blog built on Vue/Nuxt showcasing architecture, interior design and structural engineering – basically spaces.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <main>
+    <h1>This is home</h1>
+    <nuxt-link to="/posts/the-oculus">To Oculus</nuxt-link>
+  </main>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
+
+
+<script>
+  export default {
+
+  }
+</script>
---
+++ pages/posts/the-oculus.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>This is oculus</h1>
+</template>
+
+
+<style>
+
+</style>
+
+
+<script>
+  export default {
+
+  }
+</script>
