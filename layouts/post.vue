adda1cdbf5b2441a74fbc99ab29a15faf3de56de
Matthew Pence <matt@ohiosveryown.co>
2019-08-16 14:12:49 -0400

    Updating post layout one

--- layouts/post.vue
+++ layouts/post.vue
@@ -2,33 +2,39 @@
 <template>
   <main class="width">
 
+    <div class="delete mb-9">
+      <saber-link to="/">Home</saber-link>
+    </div>
+
     <!-- hero / main info -->
     <header>
       <figure>
         <div class="cover">
-          <img :src='page.assets.first_img'>
+          <img class="first-img" :src='page.assets.first_img'>
         </div>
-        <figcaption class="mt-3">
+        <figcaption>
           <h1 class=" f-px">{{ page.title }}</h1>
-          <h2 class="mb-4 f-la fs-sm uc">{{ page.architect }}</h2>
+          <h2 class="f-la fs-sm uc">{{ page.architect }}</h2>
         </figcaption>
       </figure>
     </header>
 
     <article>
       <!-- intro p -->
-      <p class="f-la fs-p taj">{{ page.first_paragraph }}</p>
+      <p class="first-paragraph f-la fs-p taj">{{ page.first_paragraph }}</p>
 
       <!-- img group -->
       <section>
-        <figure><img :src='page.assets.second_img'></figure>
-        <figure><img :src='page.assets.third_img'></figure>
+        <figure><img class="second-img" :src='page.assets.second_img'></figure>
+        <figure><img class="third-img" :src='page.assets.third_img'></figure>
       </section>
 
       <!-- lg p -->
-      <p class="f-la fs-p taj">{{ page.large_paragraph }}</p>
+      <p class="second-paragraph f-la fs-p-lg taj">{{ page.large_paragraph }}</p>
       <!-- sm /final p -->
-      <p class="f-la fs-p taj">{{ page.last_paragraph }}</p>
+      <p class="third-paragraph f-la fs-p taj">{{ page.last_paragraph }}</p>
+      <!-- credits / byline -->
+      <a :href="page.link">—{{ page.byline }}</a>
     </article>
 
     <!-- previous post -->
@@ -48,19 +54,47 @@
 <style lang="scss" scoped>
   @import '../style/grid.scss';
 
-  header img {
+  .first-paragraph {
+    margin-bottom: 3.2rem;
+    @include breakpoint(md) { width: grid-width(6); }
+  }
+
+  .second-paragraph {
+    margin-bottom: 1.6rem;
+    @include breakpoint(md) { margin-bottom: 3.2rem; }
+  }
+
+  .third-paragraph {
+    text-indent: 3ch;
+    margin-bottom: 1.6rem;
+
+    @include breakpoint(md) {
+      margin: 0 0 3.2rem grid-width(6);
+      width: grid-width(6);
+    }
+  }
+
+  .first-img {
+    margin-bottom: 2.4rem;
     object-fit: cover;
     width: 100%; max-height: 64vh;
   }
 
-  article p:first-of-type {
-    @include breakpoint(md) { width: grid-width(6); }
+  .second-img {
+    margin-bottom: 1.6rem;
+    @include breakpoint(mdl) { margin-bottom: 2.4rem; }
   }
 
+  .third-img {
+    margin-bottom: 3.2rem;
+    @include breakpoint(mdl) { margin-bottom: 4rem; }
+  }
+
+  figcaption { margin-bottom: 2.4rem; }
+
   section {
-    margin-top: 4rem;
     @include breakpoint(mdl) {
-      margin: 11.2rem 0 0 grid-width(6);
+      margin-left: grid-width(6);
       width: grid-width(6);
     }
   }
--- pages/_posts/the-oculus.md
+++ pages/_posts/the-oculus.md
@@ -3,6 +3,8 @@ title: The Oculus
 date: 2020-01-01
 layout: post
 architect: Santiago Calatrava – 2016
+byline: The New Yorker
+link: https://www.newyorker.com/culture/cultural-comment/the-best-part-of-calatravas-oculus
 
 assets:
   first_img: ./img/2020-01/01.jpg
