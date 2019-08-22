94625258372f5958c512f4e10fb38cb85acfd869
Matthew Pence <matt@ohiosveryown.co>
2019-08-22 15:30:58 -0400

    Colophon Updates

--- .saber/layouts.js
+++ .saber/layouts.js
@@ -5,12 +5,12 @@ var layouts = {}
       layouts["default"] = layout_0
       
 
-      import layout_1 from "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/layouts/colophon.vue"
-      layouts["colophon"] = layout_1
+      import layout_1 from "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/layouts/post.vue"
+      layouts["post"] = layout_1
       
 
-      import layout_2 from "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/layouts/post.vue"
-      layouts["post"] = layout_2
+      import layout_2 from "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/layouts/colophon.vue"
+      layouts["colophon"] = layout_2
       
 
     export default layouts
