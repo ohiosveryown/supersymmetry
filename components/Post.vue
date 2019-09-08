ae16754c2b07c5be99086e16a44b3fbb178006af
Matthew Pence <matt@ohiosveryown.co>
2019-09-08 10:57:45 -0400

    Began styling post component

--- components/Post.vue
+++ components/Post.vue
@@ -3,10 +3,10 @@
 
 
   <article>
-    <h3 class="f-prim fs-md uc">{{ date }}</h3>
+    <h3 class="date f-prim fs-md uc">{{ date }}</h3>
     <h1 class="f-prim fs-lg uc">{{ first_line }}</h1>
-    <h1 class="f-prim fs-lg uc">{{ second_line }}</h1>
-    <h2 class="f-sec fs-sm uc">{{ architect }}</h2>
+    <h1 class="title f-prim fs-lg uc">{{ second_line }}</h1>
+    <h2 class="architect f-sec fs-sm uc">{{ architect }}</h2>
     <saber-link :to='earl'><button>VIEW POST</button></saber-link>
     <img :src='img'/>
     <div class="cover"/>
@@ -19,6 +19,12 @@
 <!-- style -->
 <style lang='scss' scoped>
   @import '../style/grid.scss';
+
+  .date {
+    @include breakpoint(md) {
+      margin-bottom: 5.6rem;
+    }
+  }
 
   article {
     display: flex;
@@ -54,6 +60,10 @@
 <script>
   export const attributes = { layout: 'page' }
   export default {
+    beforeRouteEnter (to, from, next) {
+      console.log('before route enter')
+      next()
+    },
     props: [ 'date', 'first_line', 'second_line', 'architect', 'img', 'earl' ],
   }
 </script>
