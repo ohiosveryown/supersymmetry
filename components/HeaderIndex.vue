a669e1dd192294ec0fd0cf5b7dcecf050bc3df91
Matthew Pence <matt@ohiosveryown.co>
2019-09-08 12:58:26 -0400

    Began md styles for index

---
+++ components/HeaderIndex.vue
@@ -0,0 +1,36 @@
+<!-- layout -->
+<template>
+
+
+  <header ref='header' class="">Header</header>
+
+
+</template>
+
+
+<!-- style -->
+<style lang='scss' scoped>
+  @import '../style/grid.scss';
+
+  header {
+    position: absolute;
+    z-index: var(--z1);
+    top: 0; left: 0;
+    width: 100vw;
+    box-shadow: 20px 0 16px 0 #F0F0F0;
+    background: var(--stone);
+    background: pink;
+    transition: all 500ms ease;
+
+    @include breakpoint(mdl) {
+      width: calc(100vw * .28); height: 100%;
+    }
+  }
+
+</style>
+
+
+<!-- logic -->
+<script>
+  export default { }
+</script>
