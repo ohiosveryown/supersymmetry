ac3e86d66072a10d4bcbf60190f596ad5f949049
Matthew Pence <matt@ohiosveryown.co>
2019-09-15 20:04:22 -0400

    Colophon img test

--- components/ColophonImg.vue
+++ components/ColophonImg.vue
@@ -17,6 +17,7 @@
   @import '../style/grid.scss';
 
   img {
+    cursor: pointer;
     width: 100%; height: auto;
     object-fit: cover;
   }
@@ -33,24 +34,17 @@
       const img = document.querySelector('img')
 
       const imgs = [
-        'https://images.unsplash.com/photo-1536882240095-0379873feb4e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
+        '/_saber/images/pages/img/house-01.jpg',
         'https://images.unsplash.com/photo-1517191434949-5e90cd67d2b6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ',
         'https://images.unsplash.com/photo-1525498128493-380d1990a112?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ'
       ]
 
       img.src = imgs[0]
-      let myIndex = 1
-      let count = 0
+      let imgIndex = 1
 
       figure.addEventListener('click', () => {
-        let newImgSrc = imgs[myIndex++ % imgs.length]
-        let newImg = document.createElement('img')
-
-        figure.appendChild(newImg)
-        newImg.src = newImgSrc
-
-        count += 1
-        if (count >= 3) { newImgSrc == null }
+        let newImgSrc = imgs[imgIndex++ % imgs.length]
+        img.src = newImgSrc
       })
     }
   }
