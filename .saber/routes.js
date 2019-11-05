8bf32893f34bcf50eca5b42af499c35140ef55c9
ohiosveryown <matt@ohiosveryown.co>
2019-11-04 19:43:48 -0500

    New colophon imgs

--- .saber/routes.js
+++ .saber/routes.js
@@ -4,11 +4,11 @@
               path: "/posts/daxing-airport.html",
               meta: {
                 __relative: '_posts/daxing-airport.md',
-                __pageId: '0767cc27'
+                __pageId: '230b9e90'
               },
               component: function() {
                 
-                return import(/* webpackChunkName: "page--_posts-daxing-airport-md" */ "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/pages/_posts/daxing-airport.md?saberPage=0767cc27")
+                return import(/* webpackChunkName: "page--_posts-daxing-airport-md" */ "/Users/mpence/Desktop/development/supersymmetry/pages/_posts/daxing-airport.md?saberPage=230b9e90")
                 
               }
             },
@@ -16,11 +16,11 @@
               path: "/posts/milan-house.html",
               meta: {
                 __relative: '_posts/milan-house.md',
-                __pageId: '0ae3b66e'
+                __pageId: 'c4cbb6b6'
               },
               component: function() {
                 
-                return import(/* webpackChunkName: "page--_posts-milan-house-md" */ "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/pages/_posts/milan-house.md?saberPage=0ae3b66e")
+                return import(/* webpackChunkName: "page--_posts-milan-house-md" */ "/Users/mpence/Desktop/development/supersymmetry/pages/_posts/milan-house.md?saberPage=c4cbb6b6")
                 
               }
             },
@@ -28,11 +28,11 @@
               path: "/posts/riverside-high-school.html",
               meta: {
                 __relative: '_posts/riverside-high-school.md',
-                __pageId: '35985449'
+                __pageId: 'c280ac80'
               },
               component: function() {
                 
-                return import(/* webpackChunkName: "page--_posts-riverside-high-school-md" */ "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/pages/_posts/riverside-high-school.md?saberPage=35985449")
+                return import(/* webpackChunkName: "page--_posts-riverside-high-school-md" */ "/Users/mpence/Desktop/development/supersymmetry/pages/_posts/riverside-high-school.md?saberPage=c280ac80")
                 
               }
             },
@@ -40,11 +40,11 @@
               path: "/posts/the-oculus.html",
               meta: {
                 __relative: '_posts/the-oculus.md',
-                __pageId: '2f35a2e7'
+                __pageId: '236d11d0'
               },
               component: function() {
                 
-                return import(/* webpackChunkName: "page--_posts-the-oculus-md" */ "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/pages/_posts/the-oculus.md?saberPage=2f35a2e7")
+                return import(/* webpackChunkName: "page--_posts-the-oculus-md" */ "/Users/mpence/Desktop/development/supersymmetry/pages/_posts/the-oculus.md?saberPage=236d11d0")
                 
               }
             },
@@ -52,11 +52,11 @@
               path: "/colophon.html",
               meta: {
                 __relative: 'colophon.md',
-                __pageId: 'fb76bbc6'
+                __pageId: '5da3cdd8'
               },
               component: function() {
                 
-                return import(/* webpackChunkName: "page--colophon-md" */ "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/pages/colophon.md?saberPage=fb76bbc6")
+                return import(/* webpackChunkName: "page--colophon-md" */ "/Users/mpence/Desktop/development/supersymmetry/pages/colophon.md?saberPage=5da3cdd8")
                 
               }
             },
@@ -64,11 +64,11 @@
               path: "/",
               meta: {
                 __relative: 'index.vue',
-                __pageId: '01ccd670'
+                __pageId: '2ddb4502'
               },
               component: function() {
                 
-                return import(/* webpackChunkName: "page--index-vue" */ "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/pages/index.vue?saberPage=01ccd670")
+                return import(/* webpackChunkName: "page--index-vue" */ "/Users/mpence/Desktop/development/supersymmetry/pages/index.vue?saberPage=2ddb4502")
                 
               }
             },
@@ -78,7 +78,7 @@
         path: '*',
         name: 404,
         component: function () {
-          return import(/* webpackChunkName: "404-page" */ "/Users/pence/Desktop/projects/supersymmetry/supersymmetry-2020/node_modules/saber/vue-renderer/app/404.vue")
+          return import(/* webpackChunkName: "404-page" */ "/Users/mpence/Desktop/development/supersymmetry/node_modules/saber/vue-renderer/app/404.vue")
         }
       }
     ]
